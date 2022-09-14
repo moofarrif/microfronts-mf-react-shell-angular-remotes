d5921d4e4ed1a40b9a0e07a4e2710d85af8b07cf
Jhon Moofarrif <jhon.moofarry00@usc.edu.co>
2022-09-14 17:01:46 -0500

    initial deploy nested routes angular inside shell react
    xd

--- projects/apps/banner/src/app/app.module.ts
+++ projects/apps/banner/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './mainViews/layout/layout.component';
+import { ModalComponent } from './mainViews/modal/modal.component';
+import { FooterComponent } from './mainViews/footer/footer.component';
+import { HeaderComponent } from './mainViews/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    ModalComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ projects/apps/shell/src/components/App3Modal.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef } from "react";
+import { mount } from "app3/App3Modal";
+import { app3RoutingPrefix, shellBrowserHistory } from "../routing/constants";
+import { useNavigate } from "react-router-dom";
+
+const app3Basename = `/${app3RoutingPrefix}`;
+
+export default () => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Listen to navigation events dispatched inside app3 mfe.
+    const app3NavigationEventHandler = (event: Event) => {
+      const pathname = (event as CustomEvent<string>).detail;
+      const newPathname = `${app3Basename}${pathname}`;
+      if (newPathname === shellBrowserHistory.location.pathname) {
+        return;
+      }
+      navigate(newPathname);
+    };
+    window.addEventListener("[app3] navigated", app3NavigationEventHandler);
+
+    // Listen to navigation events in shell app to notifify app3 mfe.
+    const unlistenHistoryChanges = shellBrowserHistory.listen(
+      ({ location }) => {
+        if (location.pathname.startsWith(app3Basename)) {
+          window.dispatchEvent(
+            new CustomEvent("[shell] navigated", {
+              detail: location.pathname.replace(app3Basename, ""),
+            })
+          );
+        }
+      }
+    );
+
+    mount({
+      mountPoint: wrapperRef.current!,
+      initialPathname: shellBrowserHistory.location.pathname.replace(
+        app3Basename,
+        ""
+      ),
+    });
+
+    return () => {
+      window.removeEventListener(
+        "[app3] navigated",
+        app3NavigationEventHandler
+      );
+      unlistenHistoryChanges();
+    };
+
+    console.log("useEffect");
+  }, []);
+
+  return (
+    <div ref={wrapperRef} className="banner-app">
+      <app-modal></app-modal>
+    </div>
+  );
+};
